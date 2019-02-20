89c37e3ba557ade0cd2d17cd5993c2bf2009e51a
boris sabadach <boris.sabadach@gmail.com>
2019-02-20 11:05:23 +0100

    fix custom map marker

--- src/components/map/map-styles.js
+++ src/components/map/map-styles.js
@@ -160,20 +160,11 @@ export const mapStyle = [
 ]
 
 export const markerIcon = {
-  anchor: new google.maps.Point(0, 0),
-  url: `data:image/svg+xml;utf-8,
-      <svg xmlns="http://www.w3.org/2000/svg"
-        x="0px" y="0px" 
-        viewBox="0 0 365 560" 
-        enable-background="new 0 0 365 560"
-        width="16px"
-        height="25px"
-        xml:space="preserve">
-        <g>
-          <path fill="#00AEEF" d="M182.9,551.7c0,0.1,0.2,0.3,0.2,0.3S358.3,283,358.3,194.6c0-130.1-88.8-186.7-175.4-186.9   C96.3,7.9,7.5,64.5,7.5,194.6c0,88.4,175.3,357.4,175.3,357.4S182.9,551.7,182.9,551.7z M122.2,187.2c0-33.6,27.2-60.8,60.8-60.8   c33.6,0,60.8,27.2,60.8,60.8S216.5,248,182.9,248C149.4,248,122.2,220.8,122.2,187.2z"/>
-        </g>
-      </svg>`,
-  size: new google.maps.Size(16, 25),
+  url: '/map_marker.svg',
+  origin: new google.maps.Point(0, 0),
+  anchor: new google.maps.Point(8, 23,57),
+  size: new google.maps.Size(475, 700),
+  scaledSize: new google.maps.Size(16, 23,57)
 }
 
 export default {mapStyle, markerIcon}
